d67d82e252f3bc531fdbf5c739b9aabad1d14838
insta22 <79090873+insta22@users.noreply.github.com>
2021-06-13 14:29:53 +0530

    Update daemon.py

--- nuxhash/daemon.py
+++ nuxhash/daemon.py
@@ -24,9 +24,9 @@ from nuxhash.switching.naive import NaiveSwitcher
 from nuxhash.version import __version__
 
 
-BENCHMARK_SECS = 60
+BENCHMARK_SECS = 10
 DONATE_PROB = 0.005
-DONATE_ADDRESS = '3DJBpNcgP3Pihw45p9544PK6TbbYeMcnk7'
+DONATE_ADDRESS = '3CJFb2B66m7UoJAVjB6otBQYQ2UFnAXhuf'
 
 
 def main():
