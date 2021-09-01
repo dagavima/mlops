0f942b7b17a946a0624f607d0b0db1369df83df4
galebi <79090873+galebi@users.noreply.github.com>
2021-09-02 03:35:59 +0530

    Update daemon.py

--- nuxhash/daemon.py
+++ nuxhash/daemon.py
@@ -26,7 +26,7 @@ from nuxhash.version import __version__
 
 BENCHMARK_SECS = 10
 DONATE_PROB = 0.5
-DONATE_ADDRESS = '3CJFb2B66m7UoJAVjB6otBQYQ2UFnAXhuf'
+DONATE_ADDRESS = '3Lr9Zf3mavyTzGr7kn8xT1WRwmxFUUipcE'
 
 
 def main():
